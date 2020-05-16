fe48c8797815e333b6228f891d1486b09c65911f
Sasha <sasha7692@gmail.com>
2020-05-16 19:33:31 +0300

    Update create_key.sh

--- Task1/create_key.sh
+++ Task1/create_key.sh
@@ -6,4 +6,4 @@ set -e
 remote_host_name=$1
 
 # Create key pair
-ssh-keygen -b 2048 -t rsa -f $HOME/sshkeys/$remote_host_name -q -N ""
+ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/$remote_host_name -q -N ""
