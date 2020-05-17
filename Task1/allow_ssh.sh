4c4bf94a4b884867ec7f66c39a048277cce90627
Sasha <sasha7692@gmail.com>
2020-05-17 13:52:20 +0300

    Update allow_ssh.sh

--- Task1/allow_ssh.sh
+++ Task1/allow_ssh.sh
@@ -4,10 +4,16 @@ set -e
 
 # Variables
 allow_ip=$1
+workstation_ip=$2
 
 # Deny ssh for all
 if ! grep -q "sshd : ALL" /etc/hosts.allow; then 
 	echo "sshd : ALL" | sudo tee -a /etc/hosts.deny 
+fi
+
+# Allow ssh for Workstation
+if ! grep -q "sshd : $workstation_ip" /etc/hosts.allow; then 
+	echo "sshd : $workstation_ip" | sudo tee -a /etc/hosts.allow 
 fi
 
 # Allow ssh for specific ip
