7329602136174ff3cc297ff8373a5aa0e82cfa2a
Sasha <sasha7692@gmail.com>
2020-05-17 13:32:40 +0300

    Update create_key.sh

--- Task1/create_key.sh
+++ Task1/create_key.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-# Variables
-remote_host_name=$1
-
 # Create key pair
-ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/$remote_host_name -q -N ""
+ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N ""
