1c30589309a38bfdd2495a05c15c7cbcc23d6b1e
Sasha <sasha7692@gmail.com>
2020-05-20 12:01:35 +0300

    Update cron_script.sh

--- Task1/cron_script.sh
+++ Task1/cron_script.sh
@@ -2,11 +2,20 @@
 
 set -e
 
-# Get expiring keys
-files_string=$(echo $(find schedules/* -type f -exec grep -l $(date --date='90 days' "+%Y_%m_%d") {} \;))
+# Get expiring keys (tomorrow)
+files_string_for_new_keys=$(echo $(find schedules/* -type f -exec grep -l $(date --date='tomorrow' "+%Y_%m_%d") {} \;))
 
 # Run ssh_distributor.sh for each host
-IFS=' ' read -a files_array <<< "$files_string"
-for schedule_file in "${files_array[@]}"; do
-	bash ssh_distributor.sh $schedule_file
+IFS=' ' read -a files_array_for_new_keys <<< "$files_string_for_new_keys"
+for schedule_file in "${files_array_for_new_keys[@]}"; do
+	bash ssh_distributor.sh $schedule_file false
+done
+
+# Get expired keys (yesterday)
+files_string_for_delete_keys=$(echo $(find schedules/* -type f -exec grep -l $(date --date='yesterday' "+%Y_%m_%d") {} \;))
+
+# Run ssh_distributor.sh for each host
+IFS=' ' read -a files_array_for_delete_keys <<< "$files_string_for_delete_keys"
+for schedule_file in "${files_array_for_delete_keys[@]}"; do
+	bash ssh_distributor.sh $schedule_file true
 done
