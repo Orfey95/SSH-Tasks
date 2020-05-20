9c8f5b5c0cda4e72f0991b280f3d9d5031e6e617
Sasha <sasha7692@gmail.com>
2020-05-20 10:29:04 +0300

    Update create_key.sh

--- Task1/create_key.sh
+++ Task1/create_key.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Create key pair
-ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa_$(date --date='91 days' "+%Y_%m_%d") -q -N ""
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa_$(date --date='91 days' "+%Y_%m_%d") -q -N ""
 
 # Add new ssh key
 IdentityFile ~/.ssh/id_rsa_$(date "+%Y_%m_%d") | sudo tee -a ~/.ssh/config
